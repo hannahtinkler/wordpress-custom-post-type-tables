e650dbc891476b5a55d89483ae097e95401e3866
Hannah Tinkler <hannah.tinkler@mrbandfriends.co.uk>
2017-12-21 14:28:30 +0000

    Adds escaping of CPTs

--- config.php
+++ config.php
@@ -3,7 +3,9 @@
 global $wpdb;
 
 return [
-    'post_types' => unserialize(get_option('ctp_tables:tables_enabled')) ?: [],
+    'post_types' => array_map(function ($postType) {
+        return esc_html($postType);
+    }, unserialize(get_option('ctp_tables:tables_enabled')) ?: []),
     'default_post_table' => env('DB_PREFIX', $wpdb->prefix) . 'posts',
     'default_meta_table' => env('DB_PREFIX', $wpdb->prefix) . 'postmeta',
 ];
