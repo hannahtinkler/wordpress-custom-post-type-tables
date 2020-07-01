43208759fa2931a3b86f35622a92e172005c44a3
Elie Vischel <elie.vischel@gmail.com>
2020-07-01 12:59:50 +0200

    Update config.php to make the plugin work again

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ return [
     'post_types' => array_map(function ($postType) {
         return esc_html($postType);
     }, unserialize(get_option('ctp_tables:tables_enabled')) ?: []),
-    'default_post_table' => env('DB_PREFIX', $wpdb->prefix) . 'posts',
-    'default_meta_table' => env('DB_PREFIX', $wpdb->prefix) . 'postmeta',
+    'default_post_table' => getenv('DB_PREFIX', $wpdb->prefix) . 'wp_posts',
+    'default_meta_table' => getenv('DB_PREFIX', $wpdb->prefix) . 'wp_postmeta',
 ];
